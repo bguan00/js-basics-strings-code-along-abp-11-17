037320c2a319d7e22656c002156ac687ac599ada
Bo Guan <bguan00@gmail.com>
2017-11-11 00:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Bo Guan';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessag = weclomeMessage.toUpperCase();
